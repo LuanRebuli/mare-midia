c57669fb52705dce3f60f4f6236793d3a4e4f790
Luan <yluanpvp@gmail.com>
2024-11-04 15:23:06 -0300

    feat: create the inicial page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,85 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { MenuIcon, Smartphone } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="bg-[#3F5259] h-full">
+      <header className="flex flex-row justify-between items-center">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={"nautica-logo.svg"}
+          alt="Logo"
+          width={258}
+          height={137}
+          className="mt-2"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <Sheet>
+          <SheetTrigger asChild>
+            <button className="mr-4 bg-transparent">
+              <MenuIcon color="gray" size={40} className="active:opacity-55" />
+            </button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetTitle>Navegue pelas abas</SheetTitle>
+            <SheetHeader></SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </header>
+
+      <section className="flex flex-col items-center justify-center">
+        <div className="flex items-center justify-center">
+          <Image
+            src="/main-image.png"
+            alt="image from services concluded"
+            width={600}
+            height={600}
+            className="mr-10"
+          />
+          <Image
+            src={"rectangle.svg"}
+            alt="rectangle"
+            width={350}
+            height={350}
+            className="absolute top-56 left-5"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <h1 className="text-2xl font-bold text-white text-center">
+          ⛵ Sua marca navegando em um mar de possibilidades!
+        </h1>
+
+        <Button className="bg-gradient-to-r from-[#3F5259] to-[#888C80] w-60 h-14 rounded-full mt-5">
+          CRIE SUA FAIXADA
+        </Button>
+
+        <div className="w-[350px] h-[80px] bg-gradient-to-r from-[#3F5259F2] to-[#0D1F26] rounded-full flex flex-1 mt-10 p-5">
+          <div>
+            <Image
+              src={"whatsappicon.svg"}
+              width={89}
+              height={63}
+              alt="whatsapp"
+              className="mr-6 ml-3"
+            />
+          </div>
+          <div>
+            <p className="font-bold text-2xl text-white">WhatsApp</p>
+            <p className="text-gray-300">(41) 99639-4174</p>
+          </div>
+        </div>
+
+        <div>
+          <p className="mt-16">Sobre</p>
+        </div>
+      </section>
     </div>
   );
 }
